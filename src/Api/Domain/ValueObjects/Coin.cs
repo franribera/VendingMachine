189cfe0c8b96232a61b17e148699af0f946fc91d
Francesc Ribera Hidalgo <fribera.hidalgo@outlook.com>
2021-12-19 16:05:23 +0100

    Coin VO

---
+++ src/Api/Domain/ValueObjects/Coin.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+
+namespace Api.Domain.ValueObjects;
+
+public class Coin : ValueObject
+{
+    public static Coin Zero => new(0);
+    public static Coin FiveCent => new(5);
+    public static Coin TenCent => new(10);
+    public static Coin TwentyCent => new(20);
+    public static Coin FiftyCent => new(50);
+    public static Coin HundredCent => new(100);
+
+    public int Value { get; }
+
+    private Coin(int value)
+    {
+        Value = value;
+    }
+
+    public static bool IsAllowed(int value)
+    {
+        var allowedValues = typeof(Coin)
+            .GetProperties(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)
+            .Where(p => p.PropertyType == typeof(Coin))
+            .Select(p => p.GetValue(null))
+            .Cast<Coin>()
+            .Select(c => c.Value);
+
+        return allowedValues.Contains(value);
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
---
+++ test/Api.UnitTests/Domain/ValueObjects/CoinTests.cs
@@ -0,0 +1,48 @@
+﻿using Api.Domain.ValueObjects;
+using FluentAssertions;
+using Xunit;
+
+namespace Api.UnitTests.Domain.ValueObjects;
+
+public class CoinTests
+{
+    [Fact]
+    public void Two_coins_are_equal_if_have_same_value()
+    {
+        // Arrange
+        var coin1 = Coin.TwentyCent;
+        var coin2 = Coin.TwentyCent;
+
+        // Act - Assert
+        coin1.Should().Be(coin2);
+    }
+
+    [Fact]
+    public void Two_coins_are_not_equal_if_have_different_value()
+    {
+        // Arrange
+        var coin1 = Coin.TwentyCent;
+        var coin2 = Coin.HundredCent;
+
+        // Act - Assert
+        coin1.Should().NotBe(coin2);
+    }
+
+    [Theory]
+    [InlineData(0, true)]
+    [InlineData(1, false)]
+    [InlineData(5, true)]
+    [InlineData(10, true)]
+    [InlineData(15, false)]
+    [InlineData(20, true)]
+    [InlineData(50, true)]
+    [InlineData(100, true)]
+    public void Checks_if_coin_value_is_allowed(int value, bool allowed)
+    {
+        // Act
+        var result = Coin.IsAllowed(value);
+
+        // Assert
+        result.Should().Be(allowed);
+    }
+}
